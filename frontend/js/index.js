d4cdf8a7d56147191851cc9ccbed129fce42bf97
Hitanshu Panchal <hitanshu.panchal@atriina.com>
2022-04-28 01:04:36 +0530

    feat: CORS Error Solution, Firebase OTP Auth & Master Profile

---
+++ frontend/js/index.js
@@ -0,0 +1,137 @@
+const masterPhone = document.getElementById("masterPhone");
+const masterOtp = document.getElementById("masterOtp");
+const userPhone = document.getElementById("userPhone");
+const userOtp = document.getElementById("userOtp");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCB3CD-b0gNtSox4SY--3tECoI75j8VRLo",
+  authDomain: "qubehealth-87767.firebaseapp.com",
+  projectId: "qubehealth-87767",
+  storageBucket: "qubehealth-87767.appspot.com",
+  messagingSenderId: "543875510716",
+  appId: "1:543875510716:web:a0bcdecebb20198c52bbb9",
+  measurementId: "G-1294R73ZVG",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+let pattern = /[1-9]{1}[0-9]{9}/;
+let codePattern = /[0-9]{6}/;
+
+// ? Handle OTP Button Clicks
+$("#userOtpButton").click(function () {
+    if (userPhone.value == "") {
+        alert("User Phone Number is required");
+        return false;
+    }
+    if (!pattern.test(userPhone.value)) {
+        alert("User Phone Number should be of pattern - XXXXX12345");
+        return false;
+    }
+
+
+  // window.location.assign("./userProfile.html");
+    if (window.recaptchaVerifier != null) {
+      window.recaptchaVerifier.render().then(function (widgetId) {
+        grecaptcha.reset(widgetId);
+      });
+    }
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "userOtpButton",
+      {
+        size: "invisible",
+        callback: (response) => {
+          console.log("Captcha Verified");
+        },
+      }
+    );
+    sendVerificationCode(false);
+});
+
+$("#masterOtpButton").click(function () {
+    if (masterPhone.value == "") {
+        alert("Master Phone Number is required");
+        return false;
+    }
+    if (!pattern.test(masterPhone.value)) {
+        alert("Master Phone Number should be of pattern - XXXXX12345");
+        return false;
+    }
+
+
+  // window.location.assign("./masterProfile.html");
+    if (window.recaptchaVerifier != null) {
+      window.recaptchaVerifier.render().then(function (widgetId) {
+        grecaptcha.reset(widgetId);
+      });
+    }
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "masterOtpButton",
+      {
+        size: "invisible",
+        callback: (response) => {
+          console.log("Captcha Verified");
+        },
+      }
+    );
+    sendVerificationCode(true);
+});
+
+// ? Handle Sending Verification Code to User
+const sendVerificationCode = (isMaster) => {
+  let phoneNumber;
+  if (isMaster) {
+    phoneNumber = masterPhone.value;
+  } else {
+    phoneNumber = userPhone.value;
+  }
+  phoneNumber = "+91" + phoneNumber;
+  const appVerifier = window.recaptchaVerifier;
+  auth
+    .signInWithPhoneNumber(phoneNumber, appVerifier)
+    .then((confirmationResult) => {
+      const sentCodeId = confirmationResult.verificationId;
+      console.log("SMS Sent" + sentCodeId);
+      if (isMaster) {
+        $("#masterSubmit").click(() => {
+          signInWithPhone(sentCodeId, isMaster);
+        });
+      } else {
+        $("#userSubmit").click(() => {
+          signInWithPhone(sentCodeId, isMaster);
+        });
+      }
+    });
+};
+
+// ? Handle Signing User with Verification Code
+const signInWithPhone = (sentCodeId, isMaster) => {
+  let code;
+  if (isMaster) {
+    code = masterOtp.value;
+  } else {
+    code = userOtp.value;
+  }
+  if (!codePattern.test(code)) {
+    alert("OTP should be numeric and have a length of 6.");
+    return false;
+  }
+
+  const credential = firebase.auth.PhoneAuthProvider.credential(
+    sentCodeId,
+    code
+  );
+  auth
+    .signInWithCredential(credential)
+    .then(() => {
+      if (isMaster) {
+        window.location.assign("./masterProfile.html");
+      } else {
+        window.location.assign("./userProfile.html");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
---
+++ frontend/js/master.js
@@ -0,0 +1,51 @@
+const userPhone = document.getElementById("userPhone");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+
+$("#userSubmit").click(() => {
+
+    let pattern = /[1-9]{1}[0-9]{9}/;
+    if (userPhone.value == "") {
+        alert("User Phone Number is required");
+        return false;
+    }
+    if (!pattern.test(userPhone.value)) {
+        alert("User Phone Number should be of pattern - XXXXX12345");
+        return false;
+    }
+    if (firstName.value == "") {
+        alert("User's First Name is required");
+        return false;
+    }
+    if (lastName.value == "") {
+        alert("User's Last Name is required");
+        return false;
+    }
+    
+    let form = new FormData();
+    form.append("mobile_number", userPhone.value);
+    form.append("first_name", firstName.value);
+    form.append("last_name", lastName.value);
+
+    let settings = {
+    "url": "http://localhost:8080/users/create",
+    "method": "POST",
+    "timeout": 0,
+    "processData": false,
+    "mimeType": "multipart/form-data",
+    "contentType": false,
+    "data": form
+    };
+
+    $.ajax(settings)
+    .done(function (response) {
+        console.log("Success: " + response);
+        alert("User Added Successfully!");
+    })
+    .fail(function(e) {
+        var result = JSON.parse(JSON.stringify(e));
+        console.log(result);
+        alert("Something went wrong, Please try again.");
+    });
+
+});
